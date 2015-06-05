9718b2eb47c3ade2698ca02d4f3c9a12f7a562df
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-06-05 16:04:24 -0400

    update testsuite name

--- 4LTR_Course-Variant-MTKG9.sh
+++ 4LTR_Course-Variant-MTKG9.sh
@@ -2,5 +2,8 @@
 defhost=localhost
 HOST=${1-$defhost}
 echo Running SOAPUI TestSuite on: $HOST
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-sh testrunner.sh -e$HOST -s"SOA 4LTR MTKG9 Course Variant ProjectTestSuite" -r -a -f/ccs-soapui-tests/Reports /ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
+#cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
+cd /opt/ReadyAPI-1.2.2/bin/
+sh testrunner.sh -s "SOA_4LTR_MTKG9_Course_Variant_ProjectTestSuite" /home/cmartello/ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
+
+#sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s"SOA 4LTR MTKG9 Course Variant ProjectTestSuite" -r -a -f/ccs-soapui-tests/Reports /ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
