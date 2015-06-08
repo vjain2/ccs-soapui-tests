7d6d3ac64be6df49b22b213cba06a7a78f8414a5
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-06-08 17:48:10 -0400

    more updates to LCS.sh

--- SOA_LCS_4LTR_Master_NDF_Checker.sh
+++ SOA_LCS_4LTR_Master_NDF_Checker.sh
@@ -1,7 +1,12 @@
 # Run SoapUI test suite from Jenkin
 cd ~/SOAPUI/ccs-soapui-tests
+pwd
+
+/Users/mindtap/SOAPUI/ccs-soapui-tests
+pwd
+echo “* * * * * Test Starting * * * * *”
 git pull
 
 cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
 
-sh testrunner.sh -s”MasterNDF_Checker_TestSuite” -r -a -f/Users/mindtap/SOAPUI/ccs-soapui-tests /Users/mindtap/SOAPUI/ccs-soapui-tests/LCS-Int-4LTR-Master-NDF-checker-Project-soapui-project.xml
+sh testrunner.sh -s”MasterNDF_Checker_TestSuite” -r -a -f/Users/mindtap/SOAPUI/ccs-soapui-tests/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/LCS-Int-4LTR-Master-NDF-checker-Project-soapui-project.xml
