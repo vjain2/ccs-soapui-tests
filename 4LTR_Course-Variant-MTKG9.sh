a15ac95963a7da219861c745bc4dee92df6efd56
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-06-09 15:19:08 -0400

    update project per AWS /opt/code path

--- 4LTR_Course-Variant-MTKG9.sh
+++ 4LTR_Course-Variant-MTKG9.sh
@@ -1,9 +1,15 @@
 # Run SoapUI test suite from Jenkin with Host as parameter
-defhost=localhost
-HOST=${1-$defhost}
-echo Running SOAPUI TestSuite on: $HOST
-#cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-cd /opt/ReadyAPI-1.2.2/bin/
-sh testrunner.sh -s "SOA_4LTR_MTKG9_Course_Variant_ProjectTestSuite" /home/cmartello/ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
+cd /opt/code/ccs-soapui-tests/
 
-#sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s"SOA 4LTR MTKG9 Course Variant ProjectTestSuite" -r -a -f/ccs-soapui-tests/Reports /ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
+* * * * * * git command start * * * * * * * * * 
+
+git stash
+git pull
+
+echo * * * * * * SOAPUI tests starting  * * * * * * 
+
+#cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
+
+ 
+sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -s "SOA_4LTR_MTKG9_Course_Variant_ProjectTestSuite" /opt/code/ccs-soapui-tests/4LTR_Course-Variant-MTKG9-soapui-project.xml
+
