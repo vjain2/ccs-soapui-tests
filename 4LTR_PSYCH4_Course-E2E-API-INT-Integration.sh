1a88fce365a73587216a7b9ebda455e583f1e532
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-06-15 18:34:35 -0400

    added response time & PSYCH4.sh

---
+++ 4LTR_PSYCH4_Course-E2E-API-INT-Integration.sh
@@ -0,0 +1,29 @@
+# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
+cd /Users/mindtap/SOAPUI/ccs-soapui-tests
+git pull
+
+defhost=localhost
+HOST=${1-$defhost}
+defshost=localhost
+SHOST=${2-$defshost}
+defuser=soapui_psych4@ng.com
+USER=${3-$defuser}
+defpass=Techizen_123
+PASS=${4-$defpass}
+
+echo !!!Running Maven test suite START on: $HOST to capture Dynamic CGI from newly created Course!!!
+cd /Users/mindtap/Documents/workspace/cgi
+export PATH=$PATH:/usr/local/apache-maven/apache-maven-3.3.3/bin
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_11.jdk/Contents/Home/jre
+ls -l
+mvn -version
+mvn clean test -Denv.CBURL=https://kt-login.cengage.com/cb/login.htm -Denv.OLRURL=http://kt-olradmin.cengage.com/olr/admin/index.jsp -Denv.USER=$USER -Denv.PASS=$PASS
+echo !!!Maven test suite END’s!!!
+
+# Run SoapUI test suite from Jenkin with Host as parameter
+
+echo Running SOAPUI TestSuite on: $HOST
+cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
+sh testrunner.sh -e$SHOST -s"4LTR E2E Search TestSuite" -r -a -f/Users/mindtap/SOAPUI/ccs-soapui-tests/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/4LTR_Course-E2E_project.xml
+sh testrunner.sh -e$HOST -s"4LTR E2E API Integration ProjectTestSuite" -r -a -f/Users/mindtap/SOAPUI/ccs-soapui-tests/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/4LTR_Course-E2E_project.xml
+
