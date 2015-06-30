d15977c82cd9517dc4504a673f3cae1ce52652a5
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-06-30 07:15:28 -0400

    Gradebook QA Env test

---
+++ SOA_SoapUI_QA_Env_Gradebook_Phase2.sh
@@ -0,0 +1,14 @@
+# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
+cd /Users/mindtap/SOAPUI/ccs-soapui-tests
+git pull
+# Run SoapUI test suite from Jenkin
+defhost=localhost
+
+HOST=${1-$defhost}
+#TestSuite=${$TS-$testsuite}
+#PROJECT=${$PR-$project}
+
+echo Running SOAPUI on: $HOST #TestSuite Name as $TestSuite and PROJECT Name as $PROJECT
+cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
+
+sh testrunner.sh -s"Gradebook_QA_Env" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/Gradebook-QA-Env-phase_2-soapui-project.xml
