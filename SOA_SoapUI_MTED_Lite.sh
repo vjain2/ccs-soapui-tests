ea01b4138d02a078f33c5e0f3ebba57e297f74a0
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-03 09:19:59 -0400

    update in sh

--- SOA_SoapUI_MTED_Lite.sh
+++ SOA_SoapUI_MTED_Lite.sh
@@ -15,19 +15,9 @@ echo Running SOAPUI TestSuite on Endpoint *** : $HOST
 cd /opt/ReadyAPI-1.2.2/bin/ 
 #sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" /opt/code/ccs-soapui-tests/EDMS_Lite_Validation-soapui-project.xml
 
-sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports  /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports  /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 
 
 
-
-# Run SoapUI test suite from Jenkin with Host as parameter
-#defhost=localhost
-#HOST=${1-$defhost}
-#echo Running SOAPUI TestSuite on: $HOST
-#cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-#cd /Applications/SoapUI-5.1.3.app/Contents/java/app/bin
-
-#sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
-#sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
 
