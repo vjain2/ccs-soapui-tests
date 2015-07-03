9728d2b3815a23908d32eaf43e84127b86663b16
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-03 09:05:56 -0400

    update in sh

--- SOA_SoapUI_MTED_Lite.sh
+++ SOA_SoapUI_MTED_Lite.sh
@@ -14,9 +14,7 @@ HOST=${1-$defrost}
 echo Running SOAPUI TestSuite on Endpoint *** : $HOST
 cd /opt/ReadyAPI-1.2.2/bin/ 
 #sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" /opt/code/ccs-soapui-tests/EDMS_Lite_Validation-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Create & Copy from Master" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Actions" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Associations bet Objectives-Assessment items" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+
 sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 
@@ -29,9 +27,7 @@ sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/
 #echo Running SOAPUI TestSuite on: $HOST
 #cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
 #cd /Applications/SoapUI-5.1.3.app/Contents/java/app/bin
-#sh testrunner.sh -e$HOST -s "Create & Copy from Master" -r -a -f/Users/mindtap/source/SOAPUI/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
-#sh testrunner.sh -e$HOST -s "Actions" -r -a -f/Users/mindtap/source/SOAPUI/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
-#sh testrunner.sh -e$HOST -s "Associations bet Objectives-Assessment items" -r -a -f/Users/mindtap/source/SOAPUI/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
+
 #sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
 #sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports /Users/mindtap/source/SOAPUI/SOA-MTED-Lite-soapui-project.xml
 
