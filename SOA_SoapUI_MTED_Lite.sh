343b66ac8ddfec61a6361db925dc408919cdfaea
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-03 09:34:04 -0400

    update in sh

--- SOA_SoapUI_MTED_Lite.sh
+++ SOA_SoapUI_MTED_Lite.sh
@@ -15,8 +15,9 @@ echo Running SOAPUI TestSuite on Endpoint *** : $HOST
 cd /opt/ReadyAPI-1.2.2/bin/ 
 #sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" /opt/code/ccs-soapui-tests/EDMS_Lite_Validation-soapui-project.xml
 
-sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports  /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports  /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+
 
 
 
