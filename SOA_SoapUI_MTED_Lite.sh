2ec276555da04617a57a5e8ccb3922da161092db
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-07 07:00:04 -0400

    updated MTED Lite sh file

--- SOA_SoapUI_MTED_Lite.sh
+++ SOA_SoapUI_MTED_Lite.sh
@@ -12,25 +12,14 @@ echo * * * * * * SOAPUI tests starting  * * * * * *
 defhost=localhost
 HOST=${1-$defrost}
 echo Running SOAPUI TestSuite on Endpoint *** : $HOST
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Create & Copy from Master" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Actions" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Associations bet Objectives-Assessment items" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-
-#cd /opt/ReadyAPI-1.2.2/bin/ 
-#sh /opt/ReadyAPI-1.2.2/bin/testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" /opt/code/ccs-soapui-tests/EDMS_Lite_Validation-soapui-project.xml
+cd /opt/ReadyAPI-1.2.2/bin/ 
 <<<<<<< HEAD
-#sh testrunner.sh -e$HOST -s "Create & Copy from Master" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-#sh testrunner.sh -e$HOST -s "Actions" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-#sh testrunner.sh -e$HOST -s "Associations bet Objectives-Assessment items" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Create & Copy from Master" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Actions" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+sh testrunner.sh -e$HOST -s "Associations bet Objectives-Assessment items" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 =======
-
-sh testrunner.sh -e$HOST -s "Objectives_Bad Data" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
-sh testrunner.sh -e$HOST -s "Label_CourseID_Code_Validations" -r -a -f/Users/mindtap/SOAPUI/Reports  /Users/mindtap/SOAPUI/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
 >>>>>>> 4ee80ad07bc3c3f517c8e83135acf89eb664b59a
 
 
