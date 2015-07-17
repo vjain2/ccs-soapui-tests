d0286b05808af990d2e18044a3e1cdfd1e3331cd
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-17 13:36:01 -0400

    NEW_AWS_support1

--- PROD_HealthChecker.sh
+++ PROD_HealthChecker.sh
@@ -8,4 +8,4 @@ sudo git pull
 
 echo Running SOAPUI TestSuite on: $HOST
 
-sh /opt/SoapUI-5.2.0/bin/testrunner.sh -s"Services" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/PROD-Services-Check-soapui-project.xml
+sh /opt/SoapUI-5.2.0/bin/testrunner.sh -s"Services" -r -a /opt/code/ccs-soapui-tests/PROD-Services-Check-soapui-project.xml
