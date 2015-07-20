124a3301a9ee75d99b1a6a9ef40bad400c15e026
Vinay Thakur <Vinay.thakur@contractor.cengage.com>
2015-07-20 02:04:37 -0400

    moved to shFiles folder

--- STAGE_HealthChecker.sh
+++
@@ -1,9 +0,0 @@
-# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
-cd /Users/mindtap/SOAPUI/ccs-soapui-tests
-git pull
-
-# Run SoapUI test suite from Jenkin with Host as parameter
-
-echo Running SOAPUI TestSuite on: $HOST
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-sh testrunner.sh -s"Services" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/STAGE-Services-Check-soapui-project.xml
