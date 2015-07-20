8542e7ec00e4a3e542e90640b235d456d58db861
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-20 05:08:29 -0400

    Added

---
+++ EDMS_GB_Term_Assets_PreProd.sh
@@ -0,0 +1,21 @@
+# AWS Specific commands
+sudo cd /opt/code/ccs-soapui-tests/
+echo * * * * * * list of files * * * * * * * * *
+pwd
+echo * * * * * * git command start * * * * * * * * * 
+
+sudo git stash
+sudo git pull
+
+echo * * * * * * SOAPUI tests starting  * * * * * * 
+
+defhost=localhost
+HOST=${1-$defrost}
+echo Running SOAPUI TestSuite on Endpoint *** : $HOST
+cd /opt/ReadyAPI-1.2.2/bin/ 
+sh testrunner.sh -e$HOST -s "EDMS_GB_Term_Assets_PreProd" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOAPUI_QA_EDMSL2_GB_PreProd-soapui-project.xml
+
+
+
+
+
