55cbce833861adc36bd3b164510ca54467952d1c
Thakur <vthakur@mabosvthakur-m1.local>
2015-07-20 01:39:31 -0400

    shINT

---
+++ shFiles/INT_HealthChecker.sh
@@ -0,0 +1,11 @@
+# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
+cd /opt/code/ccs-soapui-tests/
+git pull
+
+# Run SoapUI test suite from Jenkin with Host as parameter
+
+echo Running SOAPUI TestSuite on: $HOST
+#cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
+#sh testrunner.sh -s"Services" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/INT-Services-Check-soapui-project.xml
+
+sh /opt/SoapUI-5.2.0/bin/testrunner.sh -s"Services" -r -a /opt/code/ccs-soapui-tests/INT-Services-Check-soapui-project.xml
