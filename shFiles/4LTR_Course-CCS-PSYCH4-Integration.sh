c2aaa63469bb62bb168dc6c0506f2bc971637a60
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-20 15:19:14 -0400

    addsh

--- shFiles/4LTR_Course-CCS-PSYCH4-Integration.sh
+++ shFiles/4LTR_Course-CCS-PSYCH4-Integration.sh
@@ -1,6 +1,6 @@
 # Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
-cd /Users/mindtap/SOAPUI/ccs-soapui-tests
-git pull
+#cd /Users/mindtap/SOAPUI/ccs-soapui-tests
+#git pull
 
 defhost=localhost
 HOST=${1-$defhost}
@@ -14,14 +14,13 @@ defolruser=vinay.thakur@contractor.cengage.com
 OLRUSER=${5-$defolruser}
 defolrpass=Techizen_123
 OLRPASS=${6-$defolrpass}
-defxls=/Users/mindtap/SOAPUI/ccs-soapui-tests/Data/SoapUI_STAGE_CCS_PSYCH4.xls
+defxls=/opt/code/ccs-soapui-tests/Data/SoapUI_STAGE_CCS_PSYCH4.xls
 XLS=${7-$defxls}
 #-Denv.XLS=$XLS
 
 # Run SoapUI test suite from Jenkin with Host as parameter
 
 echo Running SOAPUI TestSuite on: $HOST
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-sh testrunner.sh -e$SHOST -s"4LTR CCS Search TestSuite" -PXLPath=$XLS -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/4LTR_Course-E2E_cdf_project.xml
-sh testrunner.sh -e$HOST -s"4LTR CCS API Integration ProjectTestSuite" -PXLPath=$XLS -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/4LTR_Course-E2E_cdf_project.xml
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"4LTR CCS Search TestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-E2E_cdf_project.xml
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"4LTR CCS API Integration ProjectTestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-E2E_cdf_project.xml
 
