3274ae664dc53461f17239efd46407577e29f376
Thakur <vthakur@mabosvthakur-m1.local>
2015-07-20 02:19:13 -0400

    addRepDir

--- shFiles/INT_HealthChecker.sh
+++ shFiles/INT_HealthChecker.sh
@@ -8,4 +8,4 @@ echo Running SOAPUI TestSuite on: $HOST
 #cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
 #sh testrunner.sh -s"Services" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/INT-Services-Check-soapui-project.xml
 
-sh /opt/SoapUI-5.2.0/bin/testrunner.sh -s"Services" -r -a /opt/code/ccs-soapui-tests/INT-Services-Check-soapui-project.xml
+sh /opt/SoapUI-5.2.0/bin/testrunner.sh -s"Services" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/INT-Services-Check-soapui-project.xml
