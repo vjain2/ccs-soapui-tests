3022ebf6cfc323b21c5c2cd29d0e470ad339a359
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-21 03:18:17 -0400

    sh_updated

--- SOAPUI_QA_EDMSL2_GB.sh
+++ SOAPUI_QA_EDMSL2_GB.sh
@@ -1,14 +1,19 @@
-# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
-cd /Users/mindtap/SOAPUI/ccs-soapui-tests
-git pull
-# Run SoapUI test suite from Jenkin
+# AWS Specific commands
+sudo cd /opt/code/ccs-soapui-tests/
+echo * * * * * * list of files * * * * * * * * *
+pwd
+echo * * * * * * git command start * * * * * * * * * 
+
+sudo git stash
+sudo git pull
+
+echo * * * * * * SOAPUI tests starting  * * * * * * 
+
 defhost=localhost
+HOST=${1-$defrost}
+echo Running SOAPUI TestSuite on Endpoint *** : $HOST
+cd /opt/ReadyAPI-1.2.2/bin/ 
+sh testrunner.sh -e$HOST -s "EDMS_GB_Term_Assets" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOAPUI_QA_EDMSL_GB_Prod-soapui-project.xml
 
-HOST=${1-$defhost}
-#TestSuite=${$TS-$testsuite}
-#PROJECT=${$PR-$project}
 
-#echo Running SOAPUI on: $HOST #TestSuite Name as $TestSuite and PROJECT Name as $PROJECT
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
 
-sh testrunner.sh -s"EDMS_GB_Term_Assets" -r -a -f/Users/mindtap/SOAPUI/Reports /Users/mindtap/SOAPUI/ccs-soapui-tests/SOAPUI_QA_EDMSL2_GB-soapui-project.xml
