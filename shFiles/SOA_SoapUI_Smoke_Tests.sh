bc554da33780c08de66be480c0037b3340d287df
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-21 11:46:26 -0400

    addSh

--- shFiles/SOA_SoapUI_Smoke_Tests.sh
+++ shFiles/SOA_SoapUI_Smoke_Tests.sh
@@ -2,5 +2,5 @@
 defhost=localhost
 HOST=${1-$defhost}
 echo Running SOAPUI TestSuite on: $HOST
-cd /Applications/SoapUI-5.0.0.app/Contents/java/app/bin
-sh testrunner.sh -e$HOST -s"SOA Smoke TestSuite" -r -a -f/Users/mindtap/source/SOAPUI/Reports /Users/mindtap/source/SOAPUI/SOA-Smoke-soapui-project.xml
+
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"SOA Smoke TestSuite" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-Smoke-soapui-project.xml
