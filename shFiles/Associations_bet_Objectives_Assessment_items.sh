4939c2d0e0e8dbbbb51aaa4ea1043a507c81eea9
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-23 10:43:26 -0400

    shadded

---
+++ shFiles/Associations_bet_Objectives_Assessment_items.sh
@@ -0,0 +1,5 @@
+defhost=localhost
+HOST=${1-$defhost}
+
+Running SOAPUI TestSuite on: $HOST
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"Associations bet Objectives-Assessment items" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
