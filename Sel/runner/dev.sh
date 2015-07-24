46cb5bb6c5a2b4bc551893fb44f27273a6015cf3
Apoorva Sharma <apoorva.sharma@contractor.cengage.com>
2015-07-24 19:08:55 -0400

    addSeleniumtoAWS

---
+++ Sel/runner/dev.sh
@@ -0,0 +1,1 @@
+mvn clean test -Denv.CBURL=https://d-login.cengage.com/cb/login.htm -Denv.OLRURL=http://d-olradmin.cengage.com/olr/admin/index.jsp -Denv.USER=soapui_mktg9@ng.com -Denv.PASS=Techizen_123 -Denv.OLRUSER=vinay.thakur@contractor.cengage.com -Denv.OLRPASS=Techizen_123 -Denv.XLS=/Users/mindtap/SOAPUI/Data/SoapUI_INT_e2e_4LTR_MTKG9_Variant.xls
