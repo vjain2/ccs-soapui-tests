473e555dc410eed95b8d45b291b4c55356cbfbd4
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-07-27 09:02:29 -0400

    updated_sh_file

--- shFiles/SOA_SoapUI_INT_Env_Gradebook_Phase2.sh
+++ shFiles/SOA_SoapUI_INT_Env_Gradebook_Phase2.sh
@@ -17,7 +17,9 @@ XLS=${7-$defxls}
 # Run SoapUI test suite from Jenkin with Host as parameter
 
 echo Running SOAPUI TestSuite on: $HOST
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"Gradebook_Token_Generator" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/Gradebook_Integration_Env_Phase2_new-soapui-project.xml
 sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"Gradebook_Integration_Env" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/Gradebook_Integration_Env_Phase2_new-soapui-project.xml
+
 
 
 
