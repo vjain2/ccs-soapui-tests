ba8bd81d87ec3ebc43fee40ef4e6baf45cb4eb94
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-30 17:35:00 -0400

    Add_MVNCommand5

--- Sel/cgi/dev_delete.sh
+++ Sel/cgi/dev_delete.sh
@@ -3,10 +3,7 @@
 echo Running Maven Delete Utility…
 
 cd /opt/code/ccs-soapui-tests/Sel/cgi
-pwd
-whoami
-echo $JAVA_HOME
-echo $PATH
+sudo chown -R ec2-user:ec2-user /opt/code/ccs-soapui-tests/Sel
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.85.x86_64/jre
 export PATH=$PATH:/opt/apache-maven-3.3.3/bin:$JAVA_HOME/bin
 
