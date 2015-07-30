3c433dbc63024d765b034efa0cd29a6b2266c275
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-30 16:37:19 -0400

    addMVNCommands

--- Sel/cgi/dev_delete.sh
+++ Sel/cgi/dev_delete.sh
@@ -1,2 +1,9 @@
+#Run Maven Commands
+
+echo Running Maven Delete Utility…
+
 cd /opt/code/ccs-soapui-tests/Sel/cgi
+
+mvn -version
+
 mvn clean test -Dtest=DeleteCourse -Denv.CBURL=https://d-login.cengage.com/cb/login.htm -Denv.OLRURL=http://d-olradmin.cengage.com/olr/admin/index.jsp -Denv.USER=soapui_psych4@ng.com -Denv.PASS=Techizen_123 -Denv.OLRUSER=vinay.thakur@contractor.cengage.com -Denv.OLRPASS=Techizen_123
