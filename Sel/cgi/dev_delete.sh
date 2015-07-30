9a2a40d9c7d020ace0e5742ff73edb4494b6ffdf
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-07-30 16:31:40 -0400

    addMVNPath

--- Sel/cgi/dev_delete.sh
+++ Sel/cgi/dev_delete.sh
@@ -1,1 +1,2 @@
+cd /opt/code/ccs-soapui-tests/Sel/cgi
 mvn clean test -Dtest=DeleteCourse -Denv.CBURL=https://d-login.cengage.com/cb/login.htm -Denv.OLRURL=http://d-olradmin.cengage.com/olr/admin/index.jsp -Denv.USER=soapui_psych4@ng.com -Denv.PASS=Techizen_123 -Denv.OLRUSER=vinay.thakur@contractor.cengage.com -Denv.OLRPASS=Techizen_123
