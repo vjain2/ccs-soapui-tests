f58f641641e4dd79de17819404ae4be3e214f5a4
Vinay Thakur <Vinay.thakur@contractor.cengage.com>
2015-07-31 17:05:17 -0400

    moved to shFiles folder

--- EDMS_GB_Term_Assets_PreProd.sh
+++
@@ -1,9 +0,0 @@
-defhost=localhost
-HOST=${1-$defhost}
-defxls1=/opt/code/ccs-soapui-tests/Data/GB_PreProd.xls
-XLS1=${7-$defxls1}
-
-
-echo Running SOAPUI TestSuite on: $HOST
-sh testrunner.sh -e$HOST -s "EDMS_GB_Term_Assets_PreProd" -PXLPath=$XLS1 -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOAPUI_QA_EDMSL2_GB_PreProd-soapui-project.xml
-
