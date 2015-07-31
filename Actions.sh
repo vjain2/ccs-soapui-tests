7d0195064aa55775f2c23faf72e5506adc9d334f
Vinay Thakur <Vinay.thakur@contractor.cengage.com>
2015-07-31 16:43:09 -0400

    moved to shFiles folder

--- Actions.sh
+++
@@ -1,7 +0,0 @@
-
-
-defhost=localhost
-HOST=${1-$defhost}
-
-echo Running SOAPUI TestSuite on: $HOST
-sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"Actions" -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
