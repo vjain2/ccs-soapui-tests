8120b15a248466ca36c7a3d34cf353eb8ee3bc1d
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-08-07 03:38:30 -0400

    shupdated

--- shFiles/EDMS_GB_Term_Assets_Prod.sh
+++ shFiles/EDMS_GB_Term_Assets_Prod.sh
@@ -1,20 +1,11 @@
-# AWS Specific commands
-sudo cd /opt/code/ccs-soapui-tests/
-echo * * * * * * list of files * * * * * * * * *
-pwd
-echo * * * * * * git command start * * * * * * * * * 
-
-sudo git stash
-sudo git pull
-
-echo * * * * * * SOAPUI tests starting  * * * * * * 
-
 defhost=localhost
-HOST=${1-$defrost}
-echo Running SOAPUI TestSuite on Endpoint *** : $HOST
-cd /opt/ReadyAPI-1.2.2/bin/ 
-sh testrunner.sh -e$HOST -s"EDMS_GB_Term_Assets_Prod" -PXLPath=$XLS1 -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOAPUI_QA_EDMSL_GB_Prod-soapui-project.xml
+HOST=${1-$defhost}
+defxls1=/opt/code/ccs-soapui-tests/Data/MTED_Prod.xls
+XLS1=${7-$defxls1}
+#-Denv.XLS=$XLS
 
+# Run SoapUI test suite from Jenkin with Host as parameter
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"EDMS_GB_Term_Assets_Prod" -PXLPath=$XLS1 -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOAPUI_QA_EDMSL_GB_Prod-soapui-project.xml
 
 
 
