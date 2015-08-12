8213c680d4df45c7e29e4674f686f25566e74492
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-08-12 13:28:35 -0400

    updateParameter

--- shFiles/4LTR_PSYCH4_Course-E2E-API-PROD-Integration.sh
+++ shFiles/4LTR_PSYCH4_Course-E2E-API-PROD-Integration.sh
@@ -34,6 +34,6 @@ echo !!!Maven test suite END’s!!!
 # Run SoapUI test suite from Jenkin with Host as parameter
 
 echo Running SOAPUI TestSuite on: $HOST
-sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"4LTR E2E Search TestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-E2E_project.xml
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"4LTR E2E Search TestSuite" -PXLPath=$XLS -Ps_nar_query=$QUERY -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-E2E_project.xml
 sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"4LTR E2E API Integration ProjectTestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-E2E_project.xml
 
