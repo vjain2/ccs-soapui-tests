e8bb14a158a49e712e543e981ebb9f16b94d9d3b
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-08-19 11:06:08 -0400

    add_shfile

---
+++ shFiles/SOA_SoapUI_CBS_Prod.sh
@@ -0,0 +1,13 @@
+defhost=localhost
+HOST=${1-$defhost}
+defxls=/opt/code/ccs-soapui-tests/Data/Dev_Course_Builder.xls
+XLS=${7-$defxls}
+
+# 
+Run SoapUI test suite from Jenkin with Host as parameter
+
+echo Running SOAPUI 
+TestSuite on: $HOST
+
+
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -s"Prod_Course_Builder" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/Course-Builder-Prod-Env-soapui-project.xml
