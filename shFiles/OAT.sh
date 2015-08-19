63fb2981bb6be56899d1728606134cb8bb0c7d84
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-08-19 11:21:43 -0400

    shadded

---
+++ shFiles/OAT.sh
@@ -0,0 +1,15 @@
+defhost=localhost
+HOST=${1-$defhost}
+defxls1=/opt/code/ccs-soapui-tests/Data/OAT.xls
+XLS1=${7-$defxls1}
+#-Denv.XLS=$XLS
+
+# Run SoapUI test suite from Jenkin with Host as parameter
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"OAT" -PXLPath=$XLS1 -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/SOA-MTED-Lite-soapui-project.xml
+
+
+
+
+
+
+
