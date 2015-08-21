73ffbb5a4aadfe98d337d0fc9ef1aeef3f459f1b
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-08-21 06:38:09 -0400

    xmlshxlsadded

---
+++ shFiles/DevMath_PreProd.sh
@@ -0,0 +1,11 @@
+defhost=localhost
+HOST=${1-$defhost}
+defxls=/opt/code/ccs-soapui-tests/Data/DevMath_PreProd.xls
+XLS=${7-$defxls}
+#-Denv.XLS=$XLS
+
+# Run SoapUI test suite from Jenkin with Host as parameter
+
+echo Running SOAPUI TestSuite on: $HOST
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"EDMS_DevMath_Retrieve_associations_PreProd" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/EDMS_DevMath_Retrieve_associations_PreProd-soapui-project.xml
+
