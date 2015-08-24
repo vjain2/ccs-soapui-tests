71e9356f8a5878ec88bfc8207ad90d72ea14c2d8
Thakur <vthakur@mabosvthakur-m1.corp.local>
2015-08-24 19:02:49 -0400

    updatesh

--- shFiles/SOA_SoapUI_4LTR_Course_Services.sh
+++ shFiles/SOA_SoapUI_4LTR_Course_Services.sh
@@ -11,4 +11,4 @@ XLS=${7-$defxls}
 # Run SoapUI test suite from Jenkin with Host as parameter
 
 echo Running SOAPUI TestSuite on: $HOST
-sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$SHOST -s"SOA 4LTR Course Service TestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-Project-soapui-project.xml
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"SOA 4LTR Course Service TestSuite" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/4LTR_Course-Project-soapui-project.xml
