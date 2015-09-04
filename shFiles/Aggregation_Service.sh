e4f26b26410d0d7207ce712fc629287f5f33104c
Nischal Sharma <nischal.sharma@contractor.cengage.com>
2015-09-04 07:15:50 -0400

    shupdated

---
+++ shFiles/Aggregation_Service.sh
@@ -0,0 +1,10 @@
+# Run Maven test suite from Jenkin to capture Dynamic CGI from newly created Course
+
+defhost=http://abs-qad.cengage.info
+HOST=${1-$defhost}
+defxls=/opt/code/ccs-soapui-tests/Data/aggregation_service.xls
+XLS=${7-$defxls}
+
+
+# Run SoapUI test suite from Jenkin with Host as parameter
+sh /opt/SoapUI-5.1.3/bin/testrunner.sh -e$HOST -s"Aggregation Service" -PXLPath=$XLS -r -a -f/opt/code/Reports /opt/code/ccs-soapui-tests/Aggregation-service-soapui-project.xml
